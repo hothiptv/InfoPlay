7143cc036a5eb5be76ba94bb44d1b95e549872f3
hothiptv <hothiptvnek@gmail.com>
2026-04-10 21:27:14 +0700

    Create info.js

---
+++ info.js
@@ -0,0 +1,18 @@
+const userData = [
+    {
+        "ten": "Nguyễn Văn A",
+        "ngaySinh": "01/01/2000",
+        "truong": "Đại học Bách Khoa",
+        "ngheNghiep": "Lập trình viên",
+        "soThich": "Đọc sách, Chạy bộ",
+        "bietDanh": "A Tí"
+    },
+    {
+        "ten": "Trần Thị B",
+        "ngaySinh": "15/05/1998",
+        "truong": "Đại học Kinh tế",
+        "ngheNghiep": "Kế toán",
+        "soThich": "Nấu ăn, Du lịch",
+        "bietDanh": "Bé Bê"
+    }
+];
