e38f583fa357eeebf4c743c5c62ff465a9b097cd
hothiptv <hothiptvnek@gmail.com>
2026-04-12 18:55:27 +0700

    Update info.js

--- info.js
+++ info.js
@@ -1,10 +1,1 @@
-const userData = [
-    {
-        ten: Nguyễn Trọng An,
-        ngaySinh: 05/06/2014,
-        truong: THCS Minh Thành,
-        ngheNghiep: Học sinh,
-        soThich: Chơi game,
-        bietDanh: Lợn béo
-    }
-];
+const userData = [];
